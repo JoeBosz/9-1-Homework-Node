e97ac137673019dd08df2f6736b1f4a877ae1001
JoeBosz <joeboz2213@gmail.com>
2023-04-02 13:00:12 -0500

    fixed read me

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-# Homework
+# joe
+    
+    ## Table of Contents
+    - [Description](#description)
+    
+    ## Description
+    joe
+    
+    ## Installation
+    joe
+    
+    ## Usage
+    joe
+    
     ## License
-        whichever license is needed
-        
+    joe
+    
+    ## Contributors
+    joe
+    
+    ## Test
+    joe
+    
+    ## Github
+    joe
+    
+    ## Email
+    joe
+    
+    
     
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,54 @@
-const renderLicense = (data) => {
-    if (data.license) {
-        return `## License
-        whichever license is needed
-        `
-    }
-    else  {
-        return ""
-    }
-}
+// const renderLicense = (data) => {
+//     if (data.license) {
+//         return `## License
+//         whichever license is needed
+//         `
+//     }
+//     else  {
+//         return
+//     }
+// }
 
 
+
+// const generateMarkdown = (data) => {
+//     return `# ${data.title}
+//     ${renderLicense(data)}
+//     `;
+// }
 
 const generateMarkdown = (data) => {
     return `# ${data.title}
-    ${renderLicense(data)}
+    
+    ## Table of Contents
+    - [Description](#description)
+    
+    ## Description
+    ${data.description}
+    
+    ## Installation
+    ${data.installation}
+    
+    ## Usage
+    ${data.usage}
+    
+    ## License
+    ${data.license}
+    
+    ## Contributors
+    ${data.contributors}
+    
+    ## Test
+    ${data.test}
+    
+    ## Github
+    ${data.github}
+    
+    ## Email
+    ${data.email}
+    
+    
     `;
 }
 
-module.exports = generateMarkdown
+module.exports = generateMarkdown;
